b06bc10f8a1a128489a3073701e2fed248125303
Farzaneh Ghorbani <50452602+FarzanehGhorbani@users.noreply.github.com>
2022-07-16 19:35:19 +0430

    add changes

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -146,6 +146,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'farghorbani987@gmail.com'
-EMAIL_HOST_PASSWORD = '1570519862'
+EMAIL_HOST_PASSWORD = '********'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
